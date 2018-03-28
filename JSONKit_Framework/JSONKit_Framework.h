f510067280d78957c20759d218518273a4f5cb7f
lzh <lizhihui_yx@163.com>
2018-03-29 00:57:35 +0800

    Initial Commit

---
+++ JSONKit_Framework/JSONKit_Framework.h
@@ -0,0 +1,19 @@
+//
+//  JSONKit_Framework.h
+//  JSONKit_Framework
+//
+//  Created by lzh on 2018/3/29.
+//  Copyright © 2018年 lzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONKit_Framework.
+FOUNDATION_EXPORT double JSONKit_FrameworkVersionNumber;
+
+//! Project version string for JSONKit_Framework.
+FOUNDATION_EXPORT const unsigned char JSONKit_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONKit_Framework/PublicHeader.h>
+
+
